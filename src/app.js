175033a6d5b1e2d697462a3710676409942f80da
kaimikan <oihic@protonmail.com>
2022-03-21 22:24:52 +0100

    React-Router Setup With Server Side Linking

--- src/app.js
+++ src/app.js
@@ -1,7 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 // if you don't specify path it auto looks in node_modules
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>boilerplate</p>, document.getElementById("app"));
+const ExpenseDashboardPage = () => (
+  <div>This is from the dashboard component</div>
+);
+
+const AddExpensePage = () => <div>This is from the add component</div>;
+
+const EditExpensePage = () => <div>This is from the edit component</div>;
+
+const HelpPage = () => <div>This is from the Help component</div>;
+
+const NotFoundPage = () => (
+  <div>
+    404! <a href="/">Go Home</a>
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <div>
+        <a href="/">Home</a>
+        &nbsp;
+        <a href="/add">Add</a>
+        &nbsp;
+        <a href="/edit">Edit</a>
+        &nbsp;
+        <a href="/help">Help</a>
+      </div>
+
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/add" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
