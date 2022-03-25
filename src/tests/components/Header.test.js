7a9c589ff5b5f5c17f5fed3e09ed6eb7d64d4266
kaimikan <oihic@protonmail.com>
2022-03-25 23:32:16 +0100

    Enzyme Setup

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,18 @@
 import React from "react";
-import ReactShallowRenderer from "react-test-renderer/shallow";
+import { shallow } from "enzyme";
 import Header from "../../components/Header";
 
 test("should render Header correctly", () => {
+  const wrapper = shallow(<Header />);
+  //expect(wrapper.find("h1").text()).toBe("Expense List");
+  expect(wrapper).toMatchSnapshot();
+
+  /*
+  REACT TEST RENDERER  
   const renderer = new ReactShallowRenderer();
   renderer.render(<Header />);
   expect(renderer.getRenderOutput()).toMatchSnapshot();
   //console.log(renderer.getRenderOutput());
   //snapshot testing can be updated if a change is made and we want to keep it
+ */
 });
