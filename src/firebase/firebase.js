7e300ba3643610de4205dff17055163e3b39ba26
kaimikan <oihic@protonmail.com>
2022-04-02 01:11:19 +0200

    Separate Development & Testing Databases Setup

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,15 +13,15 @@ import {
   onChildRemoved
 } from "firebase/database";
 
+// this should not be committed since it's like the password to the database
 const firebaseConfig = {
-  apiKey: "AIzaSyBTSjUL-Jll16oxLiBAn9i0jnrSBUIytHs",
-  authDomain: "expense-list-98da6.firebaseapp.com",
-  databaseURL:
-    "https://expense-list-98da6-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "expense-list-98da6",
-  storageBucket: "expense-list-98da6.appspot.com",
-  messagingSenderId: "931141273582",
-  appId: "1:931141273582:web:3bf43e91afc1a7b46730ff"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_API_ID
 };
 
 initializeApp(firebaseConfig);
